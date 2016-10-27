2c80740dc9be606cf677ad4979f989cded6d211b
Chelsea Craig <chelsea.craig1223@gmail.com>
2016-10-27 20:21:14 +0000

    Have Knex finding famous people

--- knexLookup.js
+++ knexLookup.js
@@ -1,34 +1,64 @@
+"Use Strict"
+
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
-const knex = require("knex");
-
-const client = new knex.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  },
+  pool: {min: 1, max: 7}
 });
 
 console.log("Searching...")
 
-let inputName = process.argv.splice(2);
+const inputName = process.argv[2];
 
-knex('first_name', 'last_name', 'birthdate').from('famous_people');
+knex.select('*').from('famous_people').where('last_name', inputName)
+.then((result) => {
+  console.log(result);
+})
+
+
+var cb = []
+
+Promise.all(cb)
+.then(function () {
+  return knex.destroy();
+});
+
+
+// }).catch(function(err) {
+//   console.log(err, "There was an error");
+// }).finally(function() {
+//   knex.destroy();
+// });
+
+
+// knex('famouse_people').where({
+//   last_name: inputName
+// });
+
 
 // client.connect((err) => {
 //   if (err) {
 //     return console.error("Connection Error", err);
 //   }
-//   client.query("SELECT * FROM famous_people WHERE last_name = $1::varchar OR first_name = 'Lincoln'", [inputName], (err, result) => {
+//   console.log(`Input name '${inputName}'`);
+//   client.query("SELECT * FROM famous_people WHERE first_name = $1;", inputName, (err, result) => {
 //     if (err) {
 //       return console.error("error running query", err);
 //     }
-//     // for (var key in result) {
-//     // 	var value = result[key];
-//     // }
-//     console.log("Found " + result.rows.length + " person(s) by the name '" + inputName + "':\n" + result);
+    
+//     let famousPerson = result.rows[0];
+
+//     console.log("Found " + result.rows.length + " person(s) by the name '" + inputName)
+//     console.log(`- ${famousPerson.id}: ${famousPerson.first_name} ${famousPerson.last_name}, born '${famousPerson.birthday}'`);
 //     client.end();
 //   });
 // });
